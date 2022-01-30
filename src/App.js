42694e80b14f1125715439bc81c597eecf55474a
Tushar Mohod <tm@dummy.com>
2022-01-30 21:09:07 +0530

    merged cart feature

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import { useState } from 'react';
+
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 import Cart from './components/Cart/Cart';
 
-
 function App() {
+  
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  function showCartHandler() {
+    setCartIsShown(true);
+  }
+
+  function hideCartHandler() {
+    setCartIsShown(false);
+  }
+
   return (
     <CartProvider>
-      <Cart />
+      {cartIsShown && <Cart onClose={hideCartHandler}/>}
       <Header />
       <main>
         <Meals />
